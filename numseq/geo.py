f087ff1533a0e7613206f725b084e4082c687afe
Doug Enas <douglassenas@gmail.com>
2019-03-13 12:04:30 -0400

    completed assignment

---
+++ numseq/geo.py
@@ -0,0 +1,13 @@
+#! python3
+
+
+def square(n):
+    return n ** 2
+
+
+def triangle(n):
+    return n * (n+1) / 2
+
+
+def cube(n):
+    return n ** 3
---
+++ numseq/prime.py
@@ -0,0 +1,24 @@
+#! python3
+
+
+def primes(n):
+    all_primes = []
+    for possible_prime in range(n):
+
+        prime = True
+        for num in range(2, possible_prime):
+            if possible_prime % num == 0:
+                prime = False
+
+        if prime:
+            all_primes.append(possible_prime)
+    return all_primes
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return True
