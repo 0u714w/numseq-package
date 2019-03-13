c48682181b638dca8134e7fe0cc83dcc0992209c
Doug Enas <douglassenas@gmail.com>
2019-03-13 12:26:32 -0400

    fib

--- numseq/fib.py
+++ numseq/fib.py
@@ -1,7 +1,7 @@
 #! python3
 
 def fib(n):
-    """returns fibonacci sequence"""
+    """returns nth number of fibonacci sequence"""
     if n == 0: return 0
     elif n == 1: return 1
     else: return fib(n-1)+fib(n-2)
